0f4d964093cd504470bd793a6f2e34d42f36ce85
Unknown <paulmeyer@u.boisestate.edu>
2018-11-16 08:45:03 -0700

    Committing full game

---
+++ Assets/BallScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    Collider gameObjectCollider;
+    // Use this for initialization
+    void Start () {
+        gameObjectCollider = GetComponent<Collider>();
+        gameObjectCollider.attachedRigidbody.AddForce(0f, -100f,1f * -30);
+    }
+	
+	
+	
+}
---
+++ Assets/PaddleMovment.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovment : MonoBehaviour {
+
+    public GameObject VRhand;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = VRhand.transform.position;         //Code without locked axis
+       // transform.position.x = VRhand.position.x;       //Code with locked axis
+        //transform.position.y = VRhand.position.y;       //Code with locked axis
+
+	}
+}
